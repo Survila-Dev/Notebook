bffa414a8109e6ef904cf9c21bdc7bd1964c3c98
Survila-Dev <65458205+Survila-Dev@users.noreply.github.com>
2023-02-02 07:40:34 +0100

    add delete functionallity to notebooks

--- src/components/Notebook.tsx
+++ src/components/Notebook.tsx
@@ -3,18 +3,19 @@ import React from "react"
 interface NotebookInterface {
     id: string,
     title: string,
+    onClickHandle: React.MouseEventHandler<HTMLElement>,
 }
 
-function Notebook({ id, title }: NotebookInterface): JSX.Element {
-
-    function handleDeleteClick(e: React.FormEvent) {
-        e.preventDefault();
-        const curId = (e.target as Element).id;
-        console.log(curId);
-    }
+function Notebook({ id, title, onClickHandle }: NotebookInterface): JSX.Element {
 
     return (
-        <article className = "notebooks__notebook" onClick = {handleDeleteClick} id = {id}>{id}, {title}</article>
+        <article
+            id = {id}
+            className = "notebooks__notebook"
+            onClick = {onClickHandle}
+            >
+                {id}, {title}
+        </article>
     )
 }
 
--- src/components/NotebookSelection.tsx
+++ src/components/NotebookSelection.tsx
@@ -7,9 +7,29 @@ interface NotebookSelectionProps {
 }
 
 function NotebookSelection({ notebooks, changeNotebooks }: NotebookSelectionProps): JSX.Element {
+
+    function handleDeleteClick(e: React.FormEvent) {
+        e.preventDefault();
+        const curId = (e.target as Element).id;
+        console.log(curId);
+
+        changeNotebooks((curNotebooks) => {
+            let newNotebooks = curNotebooks;
+            newNotebooks.splice(curId as unknown as number, 1);
+            console.log(newNotebooks);
+            return newNotebooks;
+        })
+
+    }
+
     return (
         <section className = "notebooks">
-            {notebooks.map((inputString, index) => <Notebook id = {index as unknown as string} title = {inputString}/>)}
+            {notebooks.map((inputString, index) => (
+                <Notebook
+                    id = {index as unknown as string}
+                    title = {inputString}
+                    onClickHandle = {handleDeleteClick}
+                />))}
         </section>
     )
 }
