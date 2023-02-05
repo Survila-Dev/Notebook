311ea781b44cb7c8dd33dbd2efe341abf5f93188
Survila-Dev <65458205+Survila-Dev@users.noreply.github.com>
2023-02-05 12:19:34 +0100

    add title editing to notebook

--- src/components/Notebook.tsx
+++ src/components/Notebook.tsx
@@ -8,13 +8,17 @@ interface NotebookInterface {
 
 function Notebook({ id, title, onClickHandle }: NotebookInterface): JSX.Element {
 
+    const showEditor: boolean = false;
+    // {!showEditor ? <p className = "notebooks__notebook__text">{title}</p> : <input className = "notebooks__notebook__edit-title" type = "text"></input>}
+
     return (
         <article
             id = {id}
+            key = {id}
             className = "notebooks__notebook"
             onClick = {onClickHandle}
             >
-                <p className = "notebooks__notebook__text">{title}</p>
+                {!showEditor ? <p className = "notebooks__notebook__text">{title}</p> : <input className = "notebooks__notebook__edit-title" type = "text"></input>}
                 <div className = "notebooks__notebook__buttons">
                     <button className = "notebooks__notebook__rename-button">Rename</button>
                     <button className = "notebooks__notebook__delete-button">Delete</button>
