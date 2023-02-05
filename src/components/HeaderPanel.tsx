35790004b30807885049e842f409c8d7b889a918
Survila-Dev <65458205+Survila-Dev@users.noreply.github.com>
2023-02-05 11:51:24 +0100

    refactor css with variables

--- src/components/HeaderPanel.tsx
+++ src/components/HeaderPanel.tsx
@@ -4,6 +4,7 @@ function HeaderPanel(): JSX.Element {
     return (
         <section className = "header-panel">
             <h1>Notebook App</h1>
+            <p>User</p>
         </section>
     )
 }
